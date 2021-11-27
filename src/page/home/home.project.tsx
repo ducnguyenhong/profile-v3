a062dd2462421e7567462cf159839a6dd0f07946
Nguyễn Đức <autoclickvn@gmail.com>
2021-11-27 15:45:12 +0700

    Done UI projects

--- src/page/home/home.project.tsx
+++ src/page/home/home.project.tsx
@@ -22,87 +22,81 @@ export const Project: React.FC = () => {
         />
       </div>
 
-      <div className="pb-5 px-20 mt-5">
+      <div className="pb-5 px-20 mt-16">
         <div className="grid grid-cols-2 gap-x-20 gap-y-5">
-          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125">
+          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125 opacity-90 hover:opacity-100">
             <img
               src={`${process.env.PUBLIC_URL}/assets/img-project-1.png`}
               className="block w-32 h-28"
               alt="project 1"
             />
-            <label className="block mt-1 font-semibold">CVFREE</label>
+            <label className="block font-bold text-green-500">CVFREE</label>
           </div>
 
-          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125">
+          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125 opacity-90 hover:opacity-100">
             <img
               src={`${process.env.PUBLIC_URL}/assets/img-project-3.png`}
               className="block w-32 h-28"
               alt="project 1"
             />
-            <label className="block mt-1 font-semibold">CVFREE APP</label>
+            <label className="block mt-1 font-bold text-yellow-500">CVFREE APP</label>
           </div>
 
-          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125">
+          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125 opacity-90 hover:opacity-100">
             <img
               src={`${process.env.PUBLIC_URL}/assets/img-project-3.png`}
               className="block w-36 h-28"
               alt="project 1"
             />
-            <label className="block mt-1 font-semibold">CMS BASE</label>
+            <label className="block mt-1 font-bold text-yellow-500">CMS BASE</label>
           </div>
 
-          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125">
+          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125 opacity-90 hover:opacity-100">
             <img
               src={`${process.env.PUBLIC_URL}/assets/img-project-4.png`}
               className="block w-32 h-28"
               alt="project 1"
             />
-            <label className="block mt-1 font-semibold">GALAXY UTILITY</label>
+            <label className="block mt-1 font-bold text-green-600">GALAXY UTILITY</label>
           </div>
 
-          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125">
+          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125 opacity-90 hover:opacity-100">
             <img
               src={`${process.env.PUBLIC_URL}/assets/img-project-1.png`}
               className="block w-32 h-28"
               alt="project 1"
             />
-            <label className="block mt-1 font-semibold">WEATHER</label>
+            <label className="block mt-1 font-bold text-green-600">WEATHER</label>
           </div>
 
-          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125">
+          <div className="col-span-1 flex flex-col items-center cursor-pointer transform duration-300 hover:scale-125 opacity-90 hover:opacity-100">
             <img
               src={`${process.env.PUBLIC_URL}/assets/img-project-3.png`}
               className="block w-32 h-28"
               alt="project 1"
             />
-            <label className="block mt-1 font-semibold">CARDS GAME</label>
+            <label className="block mt-1 font-bold text-yellow-500">CARDS GAME</label>
           </div>
         </div>
       </div>
 
-      <div className="flex">
-        <img src={`${process.env.PUBLIC_URL}/assets/img-bottom-project-1.png`} className="block w-20" alt="umbrella" />
-        <img
-          src={`${process.env.PUBLIC_URL}/assets/img-bottom-project-2.png`}
-          className="block w-20 mx-5"
-          alt="umbrella"
-        />
-        <img
-          src={`${process.env.PUBLIC_URL}/assets/img-bottom-project-3.png`}
-          className="block w-10 h-20 mx-5"
-          alt="umbrella"
-        />
-        <img
-          src={`${process.env.PUBLIC_URL}/assets/img-bottom-project-4.png`}
-          className="block w-10 mx-5 h-12"
-          alt="umbrella"
-        />
-        <img
-          src={`${process.env.PUBLIC_URL}/assets/img-bottom-project-5.png`}
-          className="block w-16 h-14 mx-5"
-          alt="umbrella"
-        />
-      </div>
+      <img
+        src={`${process.env.PUBLIC_URL}/assets/img-bottom-project-2.png`}
+        className="w-16 absolute bottom-3 left-3 opacity-50"
+        alt="umbrella"
+      />
+
+      <img
+        src={`${process.env.PUBLIC_URL}/assets/img-bottom-project-3.png`}
+        className="absolute w-16 bottom-5 right-3 opacity-30"
+        alt="umbrella"
+      />
+
+      <img
+        src={`${process.env.PUBLIC_URL}/assets/img-bottom-project-4.png`}
+        className="w-14 h-14 absolute top-44 right-3 opacity-50"
+        alt="umbrella"
+      />
     </ProjectStyle>
   );
 };
